77588dadfa9d0f834b543deecf3ebb0d4b81329b
Ubuntu <ubuntu@ip-172-31-21-0.us-west-2.compute.internal>
2019-08-06 22:39:12 +0000

    model associations

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,2 +1,4 @@
 class Landmark < ApplicationRecord
+    has_many :walks, through: :stops
+    has_many :users, through: :favorites
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,4 @@
 class Stop < ApplicationRecord
+    belongs_to :walk 
+    belongs_to :landmark
 end
