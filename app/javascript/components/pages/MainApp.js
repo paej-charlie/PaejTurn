a259ed8690ce16321301f64e28a9719c454154eb
Ubuntu <ubuntu@ip-172-31-26-134.us-west-2.compute.internal>
2019-08-07 23:14:57 +0000

    started navbar

--- app/javascript/components/pages/MainApp.js
+++ app/javascript/components/pages/MainApp.js
@@ -1,12 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
-class Nav extends React.Component {
+
+import Header from "./Header"
+
+class MainApp extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <Header />
       </React.Fragment>
     );
   }
 }
 
-export default Nav
+export default MainApp
