cc5f54adbed541087a5709cc0a10f9e64cebe54a
Ubuntu <ubuntu@ip-172-31-21-0.us-west-2.compute.internal>
2019-08-07 23:49:11 +0000

    model validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, only: [:show]
     
+    def index 
+        render json: current_user.to_json({:include => :trips})
+    end 
+    
     def show
         render json: current_user.to_json({:include => :landmarks})
     end 
