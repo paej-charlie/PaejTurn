b338225a302884d2a09cfade038f0e0da2c114cf
Ubuntu <ubuntu@ip-172-31-94-49.ec2.internal>
2019-08-07 18:33:36 +0000

    worked on mainapp

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -1,13 +1,21 @@
-import React from "react"
-import PropTypes from "prop-types"
-class MainApp extends React.Component {
-  render () {
-    return (
+ import React from "react"
+ import PropTypes from "prop-types"
+ import { Jumbotron } from 'reactstrap'
+ import Nav from './Nav'
+ 
+ 
+ class MainApp extends React.Component {
+   render () {
+     return (
       <React.Fragment>
-        <h1>Main App</h1>
+      <Nav />
+        <Jumbotron className="headerJumbo">
+          <h1>History in Motion</h1>
+        </Jumbotron>
       </React.Fragment>
-    );
-  }
-}
+     );
+   }
+ }
 
-export default MainApp
+ export default MainApp
+
