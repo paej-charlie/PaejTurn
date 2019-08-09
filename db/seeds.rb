6ac93fbded724bee7618a05309bcbf22eaf23a79
Ubuntu <ubuntu@ip-172-31-21-0.us-west-2.compute.internal>
2019-08-09 17:47:48 +0000

    fixed commented out section of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,53 @@
+landmark_attributes = [
+    {
+        title: "The Broker's Building",
+        address: "404 Market Street",
+        city: "San Diego",
+        state: "CA",
+        zip: "92101",
+        alcohol: false,
+        latitude: "32.711797",
+        longitude: "-117.159628",
+        image: "placeholder"
+    },
+    {
+        title: "Old City Hall",
+        address: "672 Fifth Ave",
+        city: "San Diego",
+        state: "CA",
+        zip: "92101",
+        alcohol: false,
+        latitude: "32.707771",
+        longitude: "-117.159628",
+        image: "placeholder"
+    },
+    {
+        title: "Cole Block Building",
+        address: "702 Fifth Avenue",
+        city: "San Diego",
+        state: "CA",
+        zip: "92101",
+        alcohol: true,
+        latitude: "32.748568",
+        longitude: "-117.160907",
+        image: "placeholder"
+    },
+    {
+        title: "The Louis Bank of Commerce",
+        address: "835-837 5th Avenue",
+        city: "San Diego",
+        state: "CA",
+        zip: "92101",
+        alcohol: false,
+        latitude: "33.123792",
+        longitude: "-117.063208",
+        image: "placeholder"
+    },
+]
 
-    # @broker=Landmark.create(title:"The Broker's Building" , address: "404 Market Street", city: "San Diego", state: "CA", zip: "92101", alcohol: false, latitude: "32.711797", longitude: "-117.119791", image: "placeholder")
-    # @oldhall=Landmark.create(title:"Old City Hall" , address: "672 Fifth Ave", city: "San Diego", state: "CA", zip: "92101", alcohol: false, latitude: "32.707771", longitude: "-117.159628", image: "placeholder")
-    # @coleblock=Landmark.create(title:"Cole Block Building" , address: "702 Fifth Avenue", city: "San Diego", state: "CA", zip: "92101", alcohol: true, latitude: "32.748568", longitude: "-117.160907", image: "placeholder")
-    # @louisbank=Landmark.create(title:"The Louis Bank of Commerce" , address: "835-837 5th Avenue", city: "San Diego", state: "CA", zip: "92101", alcohol: false, latitude: "33.123792", longitude: "-117.063208", image: "placeholder")
-# landmark_attributes = [
-#     {
-#         title: "The Broker's Building",
-#         address: "404 Market Street",
-#         city: "San Diego",
-#         state: "CA",
-#         zip: "92101",
-#         alcohol: false,
-#         latitude: ,
-#         longitude:,
-#         image:
-#     },
-#     {
-#         title:,
-#         address:,
-#         city:,
-#         state:,
-#         zip:,
-#         alcohol:,
-#         latitude:,
-#         longitude:,
-#         image:
-#     },
-#     {
-#         title:,
-#         address:,
-#         city:,
-#         state:,
-#         zip:,
-#         alcohol:,
-#         latitude:,
-#         longitude:,
-#         image:
-#     },
-#     {
-#         title:,
-#         address:,
-#         city:,
-#         state:,
-#         zip:,
-#         alcohol:,
-#         latitude:,
-#         longitude:,
-#         image:
-#     },
-# ]
-
-# landmark_attributes.each do |attributes|
-#  Landmark.find_or_create_by(attributes)
-# end
+landmark_attributes.each do |attributes|
+ Landmark.find_or_create_by(attributes)
+end
 
 walk_attributes = [
     {
