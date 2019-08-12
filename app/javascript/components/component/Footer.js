2ab31c3eb6341a777abb5bcf9fbbf3cc448d1d6e
Amanda <amanda.macgregor13@gmail.com>
2019-08-12 17:41:34 +0000

    links changed to font color black

--- app/javascript/components/component/Footer.js
+++ app/javascript/components/component/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <footer className="footer">HiM: History in Motion  ||  PAEJ  ||  <a target='_blank' href="https://www.linkedin.com/in/paige-macgregor/">Paige MacGregor</a>  |  <a target='_blank' href="https://www.linkedin.com/in/amandamacgregor2018/">Amanda MacGregor</a>  |  <a target='_blank' href="https://www.linkedin.com/in/evan-katz-7b4aa397/">Evan Katz</a>  |  <a target='_blank' href="https://www.linkedin.com/in/jaredlemke/">Jared Lemke</a></footer>
+        <footer className="footer">HiM: History in Motion  ||  PAEJ  ||  <a target='_blank' href="https://www.linkedin.com/in/paige-macgregor/"><font color="black">Paige MacGregor</font></a>  |  <a target='_blank' href="https://www.linkedin.com/in/amandamacgregor2018/"><font color="black">Amanda MacGregor</font></a>  |  <a target='_blank' href="https://www.linkedin.com/in/evan-katz-7b4aa397/"><font color="black">Evan Katz</font></a>  |  <a target='_blank' href="https://www.linkedin.com/in/jaredlemke/"><font color="black">Jared Lemke</font></a></footer>
       </React.Fragment>
     );
   }
