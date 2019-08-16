2a5715c027535234757c41ea72a9a490296c8acb
Ubuntu <ubuntu@ip-172-31-26-134.us-west-2.compute.internal>
2019-08-16 18:43:13 +0000

    additional styling to home page

--- app/javascript/components/page/Home.js
+++ app/javascript/components/page/Home.js
@@ -111,14 +111,29 @@ class Home extends React.Component {
         
         <h2>History in Motion is a guided walking tour app with walks around downtown San Diego, California. Whether you want to checkout some historic bars, buildings, or places in history this is the app for you!</h2>
         
+        <hr></hr>
+        
+        <h3>App Features</h3>
+        
         <ul className = "app-uses">
-          <li>Curated walking tours with options for alcohol or no alcohol on your walk</li>
-          <li>Map with waypoints of specific locations on walks</li>
-          <li>Historic details of each location</li>
+          <li className = "app-uses-li">Curated walking tours with options for alcohol or no alcohol</li>
+          <li className = "app-uses-li">Responsive map with waypoints of specific locations on walks</li>
+          <li className = "app-uses-li">Historic details and facts of each location provided</li>
+        </ul>
+        
+        <hr></hr>
+        
+        <h3>San Diego Fun Facts!</h3>
+        
+        <ul className = "fun-facts">
+          <li>San Diego was known as the Tuna Capital of the World from the early 1930s to the late 1970s</li>
+          <li>Wyatt Earp ran four gambling halls in San Diego in the late 1800s</li>
+          <li>There were at least 120 transparently illegal businesses operating in the area in 1888</li>
+          <li>While Dr. Seuss was born in Massachusetts, he lived in San Diego for much of his life</li>
         </ul>
         
         <div className = "banner">
-        <h2>About Us</h2>
+        <h2 className = "banner">About Us</h2>
         </div>
         
         <div className="about-us">
@@ -126,7 +141,7 @@ class Home extends React.Component {
           <CardHeader>Paige MacGregor</CardHeader>
             <CardBody>
               <div className = "headshot-p"></div>
-              <CardText>About Paige:</CardText>
+              <CardText>Enjoys writing code, fiber arts, and playing with her cat.</CardText>
             </CardBody>
         </Card>
         
@@ -150,7 +165,7 @@ class Home extends React.Component {
           <CardHeader>Jared Lemke</CardHeader>
             <CardBody>
               <div className = "headshot-j"></div>
-              <CardText>Cool, calm and collected under pressure. </CardText>
+              <CardText>After working in the recruiting/staffing industry for 5 years, I'm looking to make the change into development while using my strong time management and organizational skills. I'm an active person with strong interests in sports and the great outdoors.</CardText>
             </CardBody>
         </Card>
         </div>
@@ -160,4 +175,3 @@ class Home extends React.Component {
 }
 
 export default Home
-// <CardImg className = "headshot-a" top width="100%" src="https://media.licdn.com/dms/image/C5603AQFef8bsUp04kA/profile-displayphoto-shrink_200_200/0?e=1571270400&v=beta&t=5oqpSsT6JAYnfFFSnA5L1pmscLyWypCmahEFZUAZIAM" alt="Amanda's Headshot" />
